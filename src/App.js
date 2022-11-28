7e5ed20ef850852be450b8d3fd8c7b7768bbdc00
thejaduijin <lokendra.06.work@gmail.com>
2022-11-28 19:10:15 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import CoinPage from './Pages/CoinPage/CoinPage';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      sdnvdfjnvofnvoen
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/coins:id" component={CoinPage} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/CoinPage/CoinPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CoinPage() {
+  return (
+    <div>
+      CoinPage
+    </div>
+  )
+}
+
+export default CoinPage
